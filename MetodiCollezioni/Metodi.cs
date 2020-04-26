7e9886c5059a03c52fb9a8a4c612b7e4e33a9c5b
luca lombardo <lucalombardo03@gmail.com>
2020-04-26 12:26:36 +0200

    Add project files.

---
+++ MetodiCollezioni/Metodi.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MetodiCollezioni
+{
+    public class Metodi
+    {
+        public static bool Uguale(string stringa1, string stringa2)
+        {
+            if (stringa1 == stringa2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
