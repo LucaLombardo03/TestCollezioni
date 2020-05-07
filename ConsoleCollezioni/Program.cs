83108d0e6d02b044063b9b9499826885cf8e0b90
luca lombardo <lucalombardo03@gmail.com>
2020-05-07 18:54:26 +0200

    add method

---
+++ ConsoleCollezioni/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using MetodiCollezioni;
+
+namespace ConsoleCollezioni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Inserisci la prima stringa: ");
+            string s1 = Console.ReadLine();
+            Console.Write("Inserisci la seconda stringa: ");
+            string s2 = Console.ReadLine();
+            bool uguaglianza = Metodi.StringheUguali(s1, s2);
+            Console.Write("Le stringhe sono uguali? " + uguaglianza);
+            
+            Console.WriteLine();
+            
+            Console.Write("Inserisci una stringa: ");
+            string s3 = Console.ReadLine();
+            bool maiuscole = Metodi.ContainsDoubleChar(s3);
+            Console.Write("La stringa contiene lettere maiuscole? " + maiuscole);  
+        }
+    }
+}
--- MetodiCollezioni/Metodi.cs
+++ MetodiCollezioni/Metodi.cs
@@ -4,9 +4,9 @@ namespace MetodiCollezioni
 {
     public class Metodi
     {
-        public static bool Uguale(string stringa1, string stringa2)
+        public static bool StringheUguali(string s1, string s2)
         {
-            if (stringa1 == stringa2)
+            if (s1 == s2)
             {
                 return true;
             }
@@ -14,6 +14,20 @@ namespace MetodiCollezioni
             {
                 return false;
             }
+        }
+        
+        public static bool ContainsDoubleChar(string s3) 
+        {
+            int maiuscole = 0;
+            for (int i = 0; i < s3.Length; i++)
+            {
+                if (Char.IsUpper(s3[i]) == true)
+                    maiuscole++;
+            }
+            if (maiuscole > 0)
+                return true;
+            else
+                return false;
         }
     }
 }
